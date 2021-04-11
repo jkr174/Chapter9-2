dbae94ce29867f4b91f6facfd44d99fd93d7cf1f
jkr174 <romo17410@gmail.com>
2021-04-11 13:31:39 -0500

    0.1
    Not allowed to open the database for some reason

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace KWSalesOrderFormProject
+{
+    partial class frmKWSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpOrder = new System.Windows.Forms.GroupBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmitOrder = new System.Windows.Forms.Button();
+            this.lblOrderID = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpOrder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpOrder
+            // 
+            this.grpOrder.Controls.Add(this.lblDate);
+            this.grpOrder.Controls.Add(this.label3);
+            this.grpOrder.Controls.Add(this.btnSubmitOrder);
+            this.grpOrder.Controls.Add(this.lblOrderID);
+            this.grpOrder.Controls.Add(this.btnExit);
+            this.grpOrder.Controls.Add(this.label1);
+            this.grpOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpOrder.Location = new System.Drawing.Point(12, 12);
+            this.grpOrder.Name = "grpOrder";
+            this.grpOrder.Size = new System.Drawing.Size(551, 240);
+            this.grpOrder.TabIndex = 0;
+            this.grpOrder.TabStop = false;
+            this.grpOrder.Text = "Order";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(6, 110);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(51, 17);
+            this.lblDate.TabIndex = 3;
+            this.lblDate.Text = "[Blank]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Order Date";
+            // 
+            // btnSubmitOrder
+            // 
+            this.btnSubmitOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmitOrder.Location = new System.Drawing.Point(389, 19);
+            this.btnSubmitOrder.Name = "btnSubmitOrder";
+            this.btnSubmitOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmitOrder.TabIndex = 1;
+            this.btnSubmitOrder.Text = "Submit";
+            this.btnSubmitOrder.UseVisualStyleBackColor = true;
+            // 
+            // lblOrderID
+            // 
+            this.lblOrderID.AutoSize = true;
+            this.lblOrderID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderID.Location = new System.Drawing.Point(6, 52);
+            this.lblOrderID.Name = "lblOrderID";
+            this.lblOrderID.Size = new System.Drawing.Size(51, 17);
+            this.lblOrderID.TabIndex = 1;
+            this.lblOrderID.Text = "[Blank]";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(470, 19);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KIDware Order";
+            // 
+            // frmKWSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.grpOrder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmKWSales";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KWSales Order Form";
+            this.Load += new System.EventHandler(this.frmKWSales_Load);
+            this.grpOrder.ResumeLayout(false);
+            this.grpOrder.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpOrder;
+        private System.Windows.Forms.Button btnSubmitOrder;
+        private System.Windows.Forms.Label lblOrderID;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KWSalesOrderFormProject
+{
+    public partial class frmKWSales : Form
+    {
+        int orderNumber;
+        SqlConnection KWSalesConnection;
+        SqlCommand ordersCommand;
+        SqlCommandBuilder ordersCommandBuilder;
+        SqlDataAdapter ordersAdapter;
+        DataTable ordersTable;
+        public frmKWSales()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void frmKWSales_Load(object sender, EventArgs e)
+        {
+            // connect to sales database
+            KWSalesConnection = new SqlConnection("Data Source =.\\SQLEXPRESS; " +
+                "AttachDbFilename = " + Application.StartupPath + "\\SQLKWSalesDB.mdf" +
+                "Integrated Security = True; " +
+                "Connect Timeout = 30; " +
+                "User Instance = True");
+            KWSalesConnection.Open();
+            // establish Orders command object
+            ordersCommand = new SqlCommand(
+                "SELECT * " +
+                "FROM Orders " +
+                "ORDER BY OrderID", KWSalesConnection);
+            // establish Orders data adapter/data table 
+            ordersAdapter = new SqlDataAdapter();
+            ordersAdapter.SelectCommand = ordersCommand;
+            ordersTable = new DataTable();
+            ordersAdapter.Fill(ordersTable);
+            orderNumber = 0;
+            NewOrder();
+        }
+        private void NewOrder()
+        {
+            string IDString;
+            DateTime thisDay = DateTime.Now;
+            lblDate.Text = thisDay.ToShortDateString();
+            // Build order ID as string
+            orderNumber++;
+            IDString = thisDay.Year.ToString().Substring(2);
+            if (thisDay.Month < 10)
+                IDString += "0" + thisDay.Month.ToString();
+            else
+                IDString += thisDay.Month.ToString();
+            if (thisDay.Day < 10)
+                IDString += "0" + thisDay.Day.ToString();
+            else
+                IDString += thisDay.Day.ToString();
+            if (orderNumber < 10)
+                IDString += "00" + orderNumber.ToString();
+            else if (orderNumber < 100)
+                IDString += "0" + orderNumber.ToString();
+            else
+                IDString += orderNumber.ToString();
+            lblOrderID.Text = IDString;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
